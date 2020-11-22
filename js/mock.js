a3153189339343ee3fe476c619ddeccabadcd395
whitehox <oketegah@gmail.com>
2020-11-22 17:00:22 +0100

    clean up

---
+++ js/mock.js
@@ -0,0 +1,384 @@
+const res = {
+  data: {
+    user: {
+      name: 'Oke Tega',
+      avatarUrl:
+        'https://avatars3.githubusercontent.com/u/21259199?u=68a41261e408ba7db216a2a10498865ff5fd50dc&v=4',
+      login: 'whitehox',
+      bio: 'I love building solutions\r\n',
+      email: 'oketegah@gmail.com',
+      company: '@decagonhq',
+      twitterUsername: 'iamcodebars_',
+      organizations: {
+        totalCount: 0,
+        edges: [],
+      },
+      status: {
+        emoji: ':house:',
+        message: 'Working from home',
+      },
+      followers: {
+        totalCount: 34,
+      },
+      following: {
+        totalCount: 48,
+      },
+      starredRepositories: {
+        totalCount: 38,
+      },
+      location: 'Lagos, Nigeria',
+      repositories: {
+        edges: [
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkzMTIzOTEyNTI=',
+              name: 'gitrepos',
+              primaryLanguage: {
+                color: '#e34c26',
+                name: 'HTML',
+              },
+              updatedAt: '2020-11-15T17:34:46Z',
+              pushedAt: '2020-11-15T17:34:44Z',
+              isPrivate: false,
+              stargazerCount: 1,
+              forkCount: 0,
+              shortDescriptionHTML: 'A clone of Github Repositories page',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkzMDMwNDA5MjQ=',
+              name: 'gforce',
+              primaryLanguage: {
+                color: '#178600',
+                name: 'C#',
+              },
+              updatedAt: '2020-10-11T04:24:02Z',
+              pushedAt: '2020-10-12T06:07:23Z',
+              isPrivate: true,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: {
+                name: 'MIT License',
+              },
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyMjAyMzU5OTA=',
+              name: 'report-exercise',
+              primaryLanguage: {
+                color: '#2b7489',
+                name: 'TypeScript',
+              },
+              updatedAt: '2019-11-07T12:51:03Z',
+              pushedAt: '2020-09-12T10:17:46Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'The driver report exercise',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxOTM5MDUxMjY=',
+              name: 'driver-report-dashboard',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-07-25T10:14:23Z',
+              pushedAt: '2020-09-12T06:20:28Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'Drive report dashboard built with react',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyODExNjI3Mjg=',
+              name: 'fs-frontend',
+              primaryLanguage: {
+                color: '#2b7489',
+                name: 'TypeScript',
+              },
+              updatedAt: '2020-07-20T16:44:27Z',
+              pushedAt: '2020-09-12T04:08:06Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyNjg0NjcyOTc=',
+              name: 'texteditor',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2020-06-01T08:38:52Z',
+              pushedAt: '2020-09-12T00:51:27Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'Simple react text editor component',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxOTc1NDc1MDM=',
+              name: 'polleon',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-09-01T18:28:18Z',
+              pushedAt: '2020-09-10T07:24:59Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxOTUwMTc4ODc=',
+              name: 'pollark',
+              primaryLanguage: {
+                color: '#2b7489',
+                name: 'TypeScript',
+              },
+              updatedAt: '2019-07-05T09:17:34Z',
+              pushedAt: '2020-09-10T02:49:01Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'An online polling system',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxOTM1ODk3MzU=',
+              name: 'react-contact-listing',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-06-25T00:24:54Z',
+              pushedAt: '2020-09-09T08:59:58Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'A Simple contact listing app using react',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxOTI4NjM5Mzk=',
+              name: 'portfolios-components',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-06-20T19:18:20Z',
+              pushedAt: '2020-09-09T08:18:01Z',
+              isPrivate: false,
+              stargazerCount: 1,
+              forkCount: 0,
+              shortDescriptionHTML: 'A simple SPA for portfolios (Learn React)',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyNjE5OTc3MTA=',
+              name: 'garamart',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2020-09-07T13:14:34Z',
+              pushedAt: '2020-09-07T13:14:32Z',
+              isPrivate: true,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxOTg3ODU1NTg=',
+              name: 'contact-app-db',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-07-25T08:00:42Z',
+              pushedAt: '2020-09-06T15:41:09Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'Contact app with mongoose and graphQL',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxODk1OTg0ODU=',
+              name: 'decagon-algorithms',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-06-14T09:41:28Z',
+              pushedAt: '2020-09-06T02:43:29Z',
+              isPrivate: false,
+              stargazerCount: 1,
+              forkCount: 0,
+              shortDescriptionHTML: 'Solutions to all my decagon algorithms',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkxODMyMTkwMjQ=',
+              name: 'pollark-model',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-07-23T12:38:01Z',
+              pushedAt: '2020-09-04T20:06:51Z',
+              isPrivate: false,
+              stargazerCount: 1,
+              forkCount: 0,
+              shortDescriptionHTML: 'A simple polling system with OOP',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyOTAyMTUxMzA=',
+              name: 'eyeson-test',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2020-09-03T13:41:51Z',
+              pushedAt: '2020-09-03T13:41:48Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyODExNjAwMTg=',
+              name: 'fs-backend',
+              primaryLanguage: {
+                color: '#2b7489',
+                name: 'TypeScript',
+              },
+              updatedAt: '2020-07-27T07:41:45Z',
+              pushedAt: '2020-07-27T07:41:47Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyODI1MTcwNDY=',
+              name: 'Account-Manager',
+              primaryLanguage: {
+                color: '#2b7489',
+                name: 'TypeScript',
+              },
+              updatedAt: '2020-07-25T20:07:56Z',
+              pushedAt: '2020-07-25T18:36:14Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML:
+                'Account manager desktop application for thenewboston.',
+              licenseInfo: {
+                name: 'MIT License',
+              },
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyODEyNDE5OTY=',
+              name: 'Website',
+              primaryLanguage: {
+                color: '#2b7489',
+                name: 'TypeScript',
+              },
+              updatedAt: '2020-07-20T22:53:45Z',
+              pushedAt: '2020-07-20T23:27:32Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML:
+                'The website and documentation for thenewboston.',
+              licenseInfo: {
+                name: 'MIT License',
+              },
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyMzQwNDQyNjI=',
+              name: 'happenings-app',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2020-01-24T08:48:19Z',
+              pushedAt: '2020-07-19T07:51:43Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: '',
+              licenseInfo: null,
+            },
+          },
+          {
+            node: {
+              id: 'MDEwOlJlcG9zaXRvcnkyMTYzNzY4MjQ=',
+              name: 'quicklyft',
+              primaryLanguage: {
+                color: '#f1e05a',
+                name: 'JavaScript',
+              },
+              updatedAt: '2019-10-20T17:58:39Z',
+              pushedAt: '2020-07-18T14:12:02Z',
+              isPrivate: false,
+              stargazerCount: 0,
+              forkCount: 0,
+              shortDescriptionHTML: 'quicklyft driver app',
+              licenseInfo: null,
+            },
+          },
+        ],
+      },
+    },
+  },
+};
